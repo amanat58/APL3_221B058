8c206b59809fe08302a374d5b5f00393ace71778
221B058 <221B058@juet.co.in>
2025-01-27 14:47:24 +0530

    le1

---
+++ le1/prob2/Main.java
@@ -0,0 +1,12 @@
+
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Mother ob=new Mother();
+        ob.show();
+        Child ob1=new Child();
+        ob1.show();
+    }
+}
